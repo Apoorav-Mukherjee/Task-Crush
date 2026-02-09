faf128f871356ee9ec0c6532f4ef8023140b99ff
Apoorav-Mukherjee <apooravmukherjee@gmail.com>
2026-02-09 10:33:56 +0530

    Initial project setup with Expo Router and TypeScript

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+            '@components': './components',
+            '@features': './features',
+            '@store': './store',
+            '@services': './services',
+            '@hooks': './hooks',
+            '@constants': './constants',
+            '@theme': './theme',
+            '@utils': './utils',
+          },
+        },
+      ],
+    ],
+  };
+};
